b606cc960409b3e8ad784784891f57b6b839b37c
Strock <nastrock@gmail.com>
2010-05-04 18:16:30 +0400

    xd_receiver

--- install.rb
+++ install.rb
@@ -5,3 +5,6 @@ vkontakte_config = File.dirname(__FILE__) + '/../../../config/vkontakte.yml'
 unless File.exist?(vkontakte_config)
 	FileUtils.copy(File.dirname(__FILE__) + '/vkontakte.yml.sample', vkontakte_config)
 end
+
+#Copy xd_receiver
+FileUtils.copy(File.dirname(__FILE__) + '/xd_receiver.html', File.dirname(__FILE__) + '/../../../public/xd_receiver.html'
